8210054dc0aff93e510ed05f81617d247973aa8a
Ash <79046086021@yandex.ru>
2026-04-09 13:52:57 +0000

    fl14

--- request_handler.cpp
+++ request_handler.cpp
@@ -1,5 +1,5 @@
 #include "request_handler.h"
-
+#include <cassert>
 /*
  * Здесь можно было бы разместить код обработчика запросов к базе, содержащего логику, которую не
  * хотелось бы помещать ни в transport_catalogue, ни в json reader.
@@ -7,8 +7,8 @@
  * Если вы затрудняетесь выбрать, что можно было бы поместить в этот файл,
  * можете оставить его пустым.
  */
-std::optional<BusStat> RequestHandler::GetBusStat(const std::string_view &bus_name) const{
-    const Bus* bus = FindRouteByName(name);
+std::optional<BusStat> RequestHandler::GetBusStat(const std::string_view name) const{
+    const Bus* bus = db_.FindRouteByName(name);
 if(bus == nullptr){
     return std::nullopt;
 }
@@ -23,13 +23,13 @@ double stop_distance=0;
 for(std::string_view stop_name : bus->stops){
     bus_set.insert(stop_name);
     stop1 = stop2;
-    stop2 = FindBusStopByName(stop_name);
+    stop2 = db_.FindBusStopByName(stop_name);
     if(flagfirst){
         flagfirst = false;
         continue;
     }
     if(stop1 != nullptr && stop2 != nullptr){
-        std::optional<double> real_dist = RealDistanceCalculator(stop1,stop2);
+        std::optional<double> real_dist = db_.RealDistanceCalculator(stop1,stop2);
         if(real_dist.has_value()){
             stop_distance = real_dist.value();
         }else{
@@ -39,8 +39,8 @@ for(std::string_view stop_name : bus->stops){
         geo_route_distance += geo::ComputeDistance(stop1->coordinates, stop2->coordinates);
         route_distance += stop_distance;
     }else{
-        if(stop1 == nullptr){ assert("stop1 nullptr");}
-        if(stop2 == nullptr){ assert("stop2 nullptr");}
+        if(stop1 == nullptr){ assert(false && "stop1 nullptr");}
+        if(stop2 == nullptr){ assert(false && "stop2 nullptr");}
     }
 }
 int uniq_stops = bus_set.size();
@@ -49,8 +49,7 @@ return BusStat{stops_on_route,uniq_stops,route_distance, curve};
 }
 
 std::optional<std::set<std::string_view> > RequestHandler::GetStopInfo(std::string_view name) const {
-
-    const Stop* stop = FindBusStopByName(name);
+    const Stop* stop = db_.FindBusStopByName(name);
     if(stop == nullptr){
         return std::nullopt;
     }
